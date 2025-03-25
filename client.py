827622a19534a4eb434ff5ddffb29653432ff750
Sharundaar <rabiller.donatien@hotmail.fr>
2025-03-25 15:10:11 -0400

    Update gitignore to ignore the token file, update client to provide proper intent and read API token from local file

--- client.py
+++ client.py
@@ -8,10 +8,13 @@ intents = discord.Intents(messages=True)
 client = discord.Client(intents=intents)
 
 @client.event
+async def on_ready():
+    print('BenBot is ready {0.user}'.format(client))
+
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    
     print(f'Message from {message.author}: {message.content}')
 
 token = open('token_DO-NOT-SUBMIT').read()
