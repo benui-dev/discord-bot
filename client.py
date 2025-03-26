befe231e3daeca8fd47729a457e47454fa284d2f
MrDrElliot <133075253+MrDrElliot@users.noreply.github.com>
2025-03-25 19:16:58 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,31 +1,18 @@
 import discord
 from discord.ext import commands
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+intents = discord.Intents.default()
+intents.messages = True
 
-intents = discord.Intents(messages=True)
-client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
-@client.event
+@bot.event
 async def on_ready():
-    print('BenBot is ready {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    print(f'Message from {message.author}: {message.content}')
+    print(f'Logged on as {bot.user}!')
 
 @bot.command()
 async def ping(ctx):
-    ctx.send('Pong!')
-    pass
+    await ctx.send('Pong!')
 
-bot.add_command(ping)
-
-token = open('token_DO-NOT-SUBMIT').read()
-client.run(token)
+token = open('token_DO-NOT-SUBMIT').read().strip()
+bot.run(token)
