4da0ce2d879dbdfb63c8a58b4ea16db1f82e2b6c
MrDrElliot <133075253+MrDrElliot@users.noreply.github.com>
2025-03-25 19:23:07 -0500

    Update client.py

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
+intents.message_content = True
 intents.messages = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
